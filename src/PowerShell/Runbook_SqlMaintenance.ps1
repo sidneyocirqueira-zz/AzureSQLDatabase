f12b9788639b23a36a39fc8ac5ce94a09d9d732e
sidneyocirqueira <42220714+sidneyocirqueira@users.noreply.github.com>
2019-08-08 18:43:58 -0300

    Update Runbook_SqlMaintenance.ps1

--- src/PowerShell/Runbook_SqlMaintenance.ps1
+++ src/PowerShell/Runbook_SqlMaintenance.ps1
@@ -1,8 +1,8 @@
-$AzureSQLServerName = "bancoscorporativos"
-$AzureSQLDatabaseName = "parceirosdb"
+$AzureSQLServerName = "informe aqui o servername"
+$AzureSQLDatabaseName = "informe aqui a database"
  
 $AzureSQLServerName = $AzureSQLServerName + ".database.windows.net"
-$Cred = Get-AutomationPSCredential -Name "SQLLogin"
+$Cred = Get-AutomationPSCredential -Name "informe aqui a credential do automation que conecta no banco de dados"
 $SQLOutput = $(Invoke-Sqlcmd -ServerInstance $AzureSQLServerName -Username $Cred.UserName -Password $Cred.GetNetworkCredential().Password -Database $AzureSQLDatabaseName -Query "exec [dbo].[AzureSQLMaintenance] @Operation='all' ,@LogToTable=1" -QueryTimeout 65535 -ConnectionTimeout 60 -Verbose) 4>&1
  
 Write-Output $SQLOutput
